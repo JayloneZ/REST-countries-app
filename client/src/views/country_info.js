128f2f0e4ed568291b81f01d1318ff7678b4baf0
Callum <callumpb@hotmail.com>
2017-12-21 10:32:00 +0000

    render country and removed bundle.js

--- client/src/views/country_info.js
+++ client/src/views/country_info.js
@@ -4,6 +4,8 @@ const CountryInfo = function () {
 
 CountryInfo.prototype.render = function (country) {
   // TODO: Render country info
+  
+  ul.appendChild(li);
 }
 
 CountryInfo.prototype.createListItem = function (label, content) {
